2353807b3295da52798c79ac75f4f1a2609de265
Nguyen Ha Viet Anh <nguyenhavietanh2503@gmail.com>
2024-02-19 11:52:16 +0700

    first crud demo using spring boot ang thymeleaf

---
+++ src/main/java/com/anhnhv/crud/model/User.java
@@ -0,0 +1,27 @@
+package com.anhnhv.crud.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+}
